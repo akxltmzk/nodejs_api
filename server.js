e021317fb8bb7ea2ed1efa9c487014ccc6724067
hyunwoo Do <hyunwoodo@hyunwooui-MacBookPro.local>
2020-03-01 17:50:53 +0900

    connect cloud mongo db

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const bootcamps = require('./routes/bootcamps')
-
-// const logger = require('./middleware/logger')
 const morgan = require('morgan')
+const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db')
 
+// load env var
 dotenv.config({path: './config/config.env'})
+
+// connect db
+connectDB()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-// app.use(logger)
+/*
+const logger = require('./middleware/logger')
+app.use(logger)
 if(process.env.NODE_ENV === 'development'){
   //@ desc 자동으로 requests정보를 콘솔에 찍어줌.
   app.use(morgan('dev'))
 }
+*/
+
 
 // mount router
 app.use('/api/v1/bootcamps',bootcamps)
 
-app.listen(PORT, console.log(`server running on ${process.env.NODE_ENV} mode on port ${PORT} `))
+const server = app.listen(PORT, console.log(`server running on ${process.env.NODE_ENV} 
+                          mode on port ${PORT} `))
 
+// handle unhandled promise rejection
+process.on('unhandledRejection',(err,promise)=>{
+  console.log(`Error : ${err.message}`)
+  // close server & exit process
+  server.close(()=>process.exit(1))
+})
