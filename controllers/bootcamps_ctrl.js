602762e65a4c5004102e7268aa6f2dd314b0c1b1
hyunwoo Do <hyunwoodo@hyunwooui-MacBookPro.local>
2020-03-23 14:17:33 +0900

    asyn/await middleware

--- controllers/bootcamps_ctrl.js
+++ controllers/bootcamps_ctrl.js
@@ -1,58 +1,43 @@
 const ErrorResponse = require('../utils/errorResponse')
 const Bootcamp = require('../models/Bootcamp_model')
+const asyncHandler = require('../middleware/async')
 
 // @desc   Get all bootcamps
 // @route  GET /api/vi/bootcamps
 // @acess  public(you dont need any token or auth something link that) 
-exports.getBootcamps = async (req,res,next)=>{
-  try{
-    const bootcamps = await Bootcamp.find()
-    res.status(200).json({succes:true,count: bootcamps.length ,data:bootcamps})
-  }
-  catch(err){
-    next(err)
-  }
-}
+exports.getBootcamps = asyncHandler(async(req,res,next)=>{
+  const bootcamps = await Bootcamp.find()
+  res.status(200).json({succes:true,count: bootcamps.length ,data:bootcamps})
+})
 
 // @desc   Get single bootcamps
 // @route  GET /api/vi/bootcamps/:id
 // @acess  public
-exports.getBootcamp = async(req,res,next)=>{
-  try{
-    const bootcamp = await Bootcamp.findById(req.params.id)
+exports.getBootcamp = asyncHandler(async(req,res,next)=>{
+  const bootcamp = await Bootcamp.findById(req.params.id)
 
-    if(!bootcamp)
-      next(err)
-
-    res.status(200).json({succes : true, data : bootcamp})
-  }
-  catch(err)
-  {
+  if(!bootcamp)
     next(err)
-  }
-}
+
+  res.status(200).json({succes : true, data : bootcamp})
+})
 
 // @desc   Create new bootcamp
 // @route  POST /api/vi/bootcamps
 // @acess  private
-exports.createBootcamp = async(req,res,next)=>{
-  try{
-    const bootcamp = await Bootcamp.create(req.body)
-    res.status(201).json({
-      succes:true,
-      data : bootcamp
-    })
-  }
-  catch(err){
-    next(err)
-  }
-}
+exports.createBootcamp = asyncHandler(async(req,res,next)=>{
+  const bootcamp = await Bootcamp.create(req.body)
+
+  res.status(201).json({
+    succes:true,
+    data : bootcamp
+  })
+})
 
 // @desc   Update bootcamp
 // @route  PUT /api/vi/bootcampss/:id
 // @acess  private
-exports.updateBootcamp = async (req,res,next)=>{
-  try{
+exports.updateBootcamp = asyncHandler(async (req,res,next)=>{
     const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id,req.body,{
       //원본이 아닌 수정 된 문서를 반환합니다. 기본값은 false
       new: true,
@@ -64,25 +49,16 @@ exports.updateBootcamp = async (req,res,next)=>{
       next(err)
    
     res.status(200).json({succes:true,data:bootcamp})
-
-  } catch(err){
-    next(err)
-  }
-}
+})
 
 // @desc   Delete bootcamp
 // @route  DELETE /api/vi/bootcampss/:id
 // @acess  private
-exports.deleteBootcamp = async (req,res,next)=>{
-  try{
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+exports.deleteBootcamp = asyncHandler(async (req,res,next)=>{
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
-    if(!bootcamp)
-      next(err)
-   
-    res.status(200).json({succes:true, data: {}})
-    
-  } catch(err){
+  if(!bootcamp)
     next(err)
-  }
-}
+  
+  res.status(200).json({succes:true, data: {}})
+})
